8fcad089011b53e76fefdf84b96a092c8a832636
kirankoyelada <kirankoyelada@gmail.com>
2017-10-19 16:11:25 +0530

    Restrict Children code

---
+++ MVCCorePratice/TagHelpers/ChildTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MVCCorePratice.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("child")]
+    public class ChildTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetContent("Child Tag helper");
+        }
+    }
+}
---
+++ MVCCorePratice/TagHelpers/ParentTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MVCCorePratice.TagHelpers
+{
+    [RestrictChildren("parent")]
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    //[HtmlTargetElement("parent")]
+    public class ParentTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetContent("Parent Tag Helper");
+        }
+    }
+}
